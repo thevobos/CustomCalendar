479bc65cc6c1336df3ac538552ad36fcd48fe686
Cengiz AKCAN <thevobos@gmail.com>
2021-08-07 11:57:33 +0300

    Update CustomCalendar.js

--- CustomCalendar.js
+++ CustomCalendar.js
@@ -1,10 +1,12 @@
 import React, {useState} from "react";
 import { Platform, Text,View, Modal, TouchableNativeFeedback } from "react-native";
 import RNDateTimePicker from '@react-native-community/datetimepicker';
+import moment from "moment";
 
 const CustomCalendar = (props) => {
 
     const [show, setShow] = useState(false);
+    const [date, setDate] = useState(new Date());
 
     React.useEffect(()=>{
 
@@ -21,22 +23,25 @@ const CustomCalendar = (props) => {
                 onRequestClose={() => {
                     props.onClose(false);
                 }}>
-                <View style={{width:"100%", height:"100%", shadowColor: "#000", shadowOffset: { width: 0, height: 2, }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5, position:"relative"}} >
+                <View style={{width:"100%", height:"100%",  backgroundColor:"rgba(0,0,0,0.47)", position:"relative"}} >
                     <View style={{width:"100%", minHeight: 330, backgroundColor:"white", position:"absolute", bottom:0, borderTopLeftRadius: 35, borderTopRightRadius: 35}}>
                         <View style={{paddingLeft:20, paddingRight:20, paddingTop:20 }}>
                             <RNDateTimePicker
                                 themeVariant="light"
-                                value={new Date()}
-                                display="spinner"
+                                value={date}
                                 locale="tr-TR"
+                                mode={"date"}
+                                display="spinner"
                                 onChange={(event, selectedDate)=>{
-                                    const currentDate = selectedDate || new Date();
-                                    props.onChange(currentDate);
+                                    const currentDate = selectedDate || date;
+                                    setDate(currentDate);
+                                    props.onChange(moment(currentDate).format("MM/DD/YYYY"));
                                 }}
                             />
                         </View>
                         <View style={{ marginTop:20, width:"100%", height:50, justifyContent:"center", alignItems:"center" }}>
-                            <TouchableNativeFeedback onPress={()=>{
+                            <TouchableNativeFeedback
+                                background={TouchableNativeFeedback.Ripple('transparent')} onPress={()=>{
                                 props.onClose(false)
                             }}>
                                 <View style={{ width:"90%", height:50, backgroundColor:"#f4f6f7", borderRadius:10, justifyContent:"center", alignItems:"center"}}>
@@ -54,13 +59,14 @@ const CustomCalendar = (props) => {
                 show &&
                 <RNDateTimePicker
                     themeVariant="light"
-                    value={new Date()}
+                    value={date}
                     locale="tr-TR"
                     display={Platform.OS === 'ios' ? 'spinner' : 'default'}
                     onChange={(event, selectedDate)=>{
+                        const currentDate = selectedDate || date;
+                        setDate(currentDate);
+                        props.onChange(moment(currentDate).format("MM/DD/YYYY"));
                         props.onClose(false);
-                        const currentDate = selectedDate || new Date();
-                        props.onChange(currentDate);
                     }}
                 />
             }
